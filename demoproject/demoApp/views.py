8aca526df8e7b9f5c0ca52f7f0e8f5c9d1d65326
Yousuf Syed <86584414+Yousuf200@users.noreply.github.com>
2022-10-17 15:51:16 +0530

    added app

---
+++ demoproject/demoApp/views.py
@@ -0,0 +1,9 @@
+from re import template
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    template = "main/index.html"
+
+    return render(request, template)
